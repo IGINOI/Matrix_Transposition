d0471df7565b7b1e40230547d37acdfb80ed3575
Davide <davidefacchinelli2003@gmail.com>
2025-01-14 09:19:23 +0100

    First version of MPI transposition that actually works

--- transposition_MPI.c
+++ transposition_MPI.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <mpi.h>
 #include <time.h>
 #ifdef _WIN32
@@ -9,10 +10,15 @@
 #endif
 
 void initializeMatrix(float *matrix, int n);
-void printMatrix(float *matrix, int n);
-int matrix_actually_transposed(float *matrix, float *transpose, int n);
+void printFlatMatrix(float *matrix, int n);
+void printNormalMatrix(float **matrix, int n);
+int matrix_actually_transposed(float *matrix, float **transpose, int n);
 
 int main(int argc, char *argv[]) {
+
+    /////////////////////////////////
+    // INITIALIZATION OF VARIABLES //
+    /////////////////////////////////
     MPI_Init(&argc, &argv);
 
     int rank, size;
@@ -24,36 +30,59 @@ int main(int argc, char *argv[]) {
             printf("Please provide a matrix size as an argument.\n");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
+
+        if (atoi(argv[1])%size != 0) {
+            printf("Matrix size must be divisible by the number of processes.\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
     }
 
     int exponent = atoi(argv[1]);
-    if (exponent < 4 || exponent > 12) {
+    if (exponent < 2 || exponent > 12) {
         if (rank == 0) {
             printf("Matrix size exponent must be between 4 and 12 (base is 2).\n");
         }
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
+    // Matrix size computation
     int matrix_size = 1 << exponent;
+    // Number of rows per process
     int local_rows = matrix_size / size;
-
-    
-
-    float *M = NULL;
-    float *T = NULL;
-    float *TT = NULL;
 
     if (rank == 0) {
         printf("The number of processes is %d, the number of rows per process is %d, and the matrix size is %d.\n", size, local_rows, matrix_size);
+    }
+
+    
+    //////////////////////////
+    // MATRICES ALLOCATIONS //
+    //////////////////////////
+
+    // Only rank 0 needs this matrix
+    // Initial matrix
+    float *M = NULL;
+    float **T = (float **)malloc(matrix_size * sizeof(float *));
+
+    if (rank == 0) {
         M = malloc(matrix_size * matrix_size * sizeof(float));
-        T = malloc(matrix_size * matrix_size * sizeof(float));
-        TT = malloc(matrix_size * matrix_size * sizeof(float));
+        for (int i = 0; i < matrix_size; i++) {
+            T[i] = (float *)malloc(matrix_size * sizeof(float));
+        }
         initializeMatrix(M, matrix_size);
     }
 
+    //Every process need a local matrix, local transpose and the gathered matrix
     float *local_matrix = malloc(local_rows * matrix_size * sizeof(float));
-    float *local_transpose = malloc(local_rows * matrix_size * sizeof(float));
+    float **local_transpose = (float **)malloc(matrix_size * sizeof(float *));
 
+    for (int i = 0; i < matrix_size; i++) {
+        local_transpose[i] = (float *)malloc(local_rows * sizeof(float));
+    }
+
+    ///////////////////////////
+    // SCATTERING THE MATRIX //
+    ///////////////////////////
     MPI_Scatter(M, local_rows * matrix_size, MPI_FLOAT, local_matrix, local_rows * matrix_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -64,7 +93,7 @@ int main(int argc, char *argv[]) {
             printf("Rank %d\n", rank);
             for (int i = 0; i < local_rows; i++) {
                 for (int j = 0; j < matrix_size; j++) {
-                    printf("%6.2f", local_matrix[i*matrix_size+j]);
+                    printf("%6.2f", local_matrix[i * matrix_size + j]);
                 }
                 printf("\n");
             }
@@ -72,20 +101,23 @@ int main(int argc, char *argv[]) {
         MPI_Barrier(MPI_COMM_WORLD);
     }
 
-    // Transpose locally
-    for (int i = 0; i < local_rows; i++) {
-        for (int j = 0; j < matrix_size; j++) {
-            local_transpose[j * local_rows + i] = local_matrix[i * matrix_size + j];
+    ////////////////////////////////
+    // LOCAL MATRIX TRANSPOSITION //
+    ////////////////////////////////
+    for (int i = 0; i < matrix_size; i++) {
+        for (int j = 0; j < local_rows; j++) {
+            local_transpose[i][j] = local_matrix[j * matrix_size + i];
         }
     }
-
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     //Printing local transposed matrices to see if they are correctly transposed
     for (int i = 0; i < size; i++) {
         if (rank == i) {
             printf("Rank %d\n", rank);
             for (int i = 0; i < matrix_size; i++) {
                 for (int j = 0; j < local_rows; j++) {
-                    printf("%6.2f", local_transpose[j+local_rows*i]);
+                    printf("%6.2f", local_transpose[i][j]);
                 }
                 printf("\n");
             }
@@ -93,38 +125,63 @@ int main(int argc, char *argv[]) {
         MPI_Barrier(MPI_COMM_WORLD);
     }
 
-    printf("Rank %d is at step 1\n", rank);
+    printf("RANK %d PASSED BARRIER 1 \n", rank);
 
-    MPI_Gather(local_transpose, local_rows * matrix_size, MPI_FLOAT, T, local_rows * matrix_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    /////////////////////////////////////
+    // GATHERING PARTIAL TRANSPOSITION //
+    /////////////////////////////////////
+    for(int i = 0; i < matrix_size; i++) {
+        MPI_Gather(local_transpose[i], local_rows , MPI_FLOAT, T[i], local_rows, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    }
 
-    printf("Rank %d is at step 2\n", rank);
+    printf("RANK %d PASSED BARRIER 2 \n", rank);
+    MPI_Barrier(MPI_COMM_WORLD);
 
+    ///////////////////
+    // CHECK PROCESS //
+    ///////////////////
+    
     if (rank == 0) {
+        printf("RANK 0 REACHED CHECKING PART \n", rank);
+
         printf("Original Matrix:\n");
-        printMatrix(M, matrix_size);
-        printf("Unordered Matrix:\n");
-        printMatrix(T, matrix_size);
+        printFlatMatrix(M, matrix_size);
         printf("Transposed Matrix:\n");
-        printMatrix(TT, matrix_size);
+        printNormalMatrix(T, matrix_size);
 
-        for (int i = 0; i < matrix_size; ++i) {
-            for (int j = 0; j < matrix_size; ++j) {
-                TT[i * matrix_size + j] = T[j * matrix_size + i];
-            }
-        }
-
-        if (matrix_actually_transposed(M, TT, matrix_size)) {
+        if (matrix_actually_transposed(M, T, matrix_size)) {
             printf("Matrix transposed successfully.\n");
         } else {
             printf("Matrix transposition failed.\n");
         }
+    }
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //////////////////
+    // FREE PROCESS //
+    //////////////////
+    printf("RANK %d REACED FREEING MEMORY PART \n", rank);
+
+    for (int i = 0; i < matrix_size; i++) {
+        free(local_transpose[i]);
+    }
+    free(local_transpose);
+    free(local_matrix);
+
+
+    if(rank == 0) {
+        for (int i = 0; i < matrix_size; i++) {
+            free(T[i]);
+        }
         free(M);
-        free(T);
-        free(TT);
     }
 
-    free(local_matrix);
-    free(local_transpose);
+    free(T);
+
+    //////////////////
+    // MPI FINILIZE //
+    //////////////////
 
     MPI_Finalize();
     return 0;
@@ -138,7 +195,7 @@ void initializeMatrix(float *matrix, int n) {
     }
 }
 
-void printMatrix(float *matrix, int n) {
+void printFlatMatrix(float *matrix, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%6.2f ", matrix[i*n+j]);
@@ -147,11 +204,21 @@ void printMatrix(float *matrix, int n) {
     }
 }
 
-int matrix_actually_transposed(float *matrix, float *transpose, int n) {
+void printNormalMatrix(float **matrix, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (matrix[i*n+j] != transpose[j*n+i]) {
+            printf("%6.2f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int matrix_actually_transposed(float *matrix, float **transpose, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i*n+j] != transpose[j][i]) {
                 return 0;
+                printf("The value [%d][%d] in the original matrix is not equal to the value [%d][%d] in the transposed matrix.", i, j, j, i);
             }
         }
     }
